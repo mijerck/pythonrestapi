926eb2ed20c443b817ad504284a4fa3a38cfa3b6
mzanda <mijerck@gmail.com>
2019-12-13 01:35:37 +0300

    management command added and database connection :tada:

--- app/core/admin.py
+++ app/core/admin.py
@@ -6,23 +6,23 @@ from core import models
 
 
 class UserAdmin(BaseUserAdmin):
-   list_display = ['name', 'email']
-   ordering = ['id']
-   fieldsets = (
-      (None, {'fields': ('email', 'password')}),
-      (_('Personal Info'), {'fields': ('name', )}),
-      (
-         _('Permission'),
-         {'fields': ('is_active', 'is_staff', 'is_superuser')}
-      ),
-      (_('Important date'), {'fields': ('last_login',)}),
-   )
-   add_fieldsets = (
-      (None, {
-         'classes': ('wide',),
-         'fields': ('email', 'password1', 'password2')
-      }),
-   )
+    list_display = ['email', 'name']
+    ordering = ['id']
+    fieldsets = (
+       (None, {'fields': ('email', 'password')}),
+       (_('Personal Info'), {'fields': ('name', )}),
+       (
+          _('Permission'),
+          {'fields': ('is_active', 'is_staff', 'is_superuser')}
+       ),
+       (_('Important date'), {'fields': ('last_login',)}),
+    )
+    add_fieldsets = (
+       (None, {
+          'classes': ('wide',),
+          'fields': ('email', 'password1', 'password2')
+       }),
+    )
 
 
 admin.site.register(models.User, UserAdmin)
